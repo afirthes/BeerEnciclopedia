c2b39b93fde2ebe9ce1765ebfe4790a8283edd75
Sergio <afirthes@gmail.com>
2023-02-24 09:27:52 +0300

    Initial Commit

---
+++ BeerEnciclopedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeerEnciclopedia
+//
+//  Created by Afir Thes on 24.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
